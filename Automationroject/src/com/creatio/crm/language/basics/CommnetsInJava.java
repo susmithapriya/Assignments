d01a569995b620961f4242ffaf6c82b5bd9ade19
~Susmitha <susmithapriya.sidigonde@gmail.com>
2025-05-24 19:09:56 +0530

    Datatype assignment

---
+++ Automationroject/src/com/creatio/crm/language/basics/CommnetsInJava.java
@@ -0,0 +1,20 @@
+package com.creatio.crm.language.basics;
+
+import java.lang.System.Logger;
+
+public class CommnetsInJava {
+
+	
+		// TODO Auto-generated constructor stub
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			System.out.println("Hello World!");
+			
+		//**********//
+		
+		/*
+		 * 
+		 */
+	}
+
+}
---
+++ Automationroject/src/com/creatio/crm/language/basics/package-info.java
@@ -0,0 +1,1 @@
+package com.creatio.crm.language.basics;
