a357045f2dde7059d3103700f36ee70b8f6cf477
~Susmitha <susmithapriya.sidigonde@gmail.com>
2025-05-26 15:56:04 +0530

    variable assighnments

--- Assignments/src/javaassignments/Assignment1_DataTypes.java
+++ Assignments/src/javaassignments/Assignment1_DataTypes.java
@@ -19,9 +19,75 @@ public class Assignment1_DataTypes {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+
 		// TODO Auto-generated method stub
-		String phonenum="123-456-7890";
-		System.out.println("Mobile number is "+phonenum);
+		String phonenum = "123-456-7890";
+		System.out.println("Mobile number is " + phonenum);
+
+		float Temperature = 25.5f;
+		System.out.println("Temperature of a city in degrees Celsius:" + Temperature);
+
+		boolean t = true;
+		boolean f = false;
+		System.out.println("Whether a customer has placed an order:" + t + " or " + f);
+
+		float money = 1000.50f;
+		System.out.println("Amount of money in a customer's bank account:" + money);
+
+		String email = "john.doe@example.com";
+		System.out.println("Person's email address: " + email);
+
+		double latitude = 37.7749;
+		double longitude = -122.4194;
+		System.out.println("Coordinates of a location (latitude, longitude):" + latitude + "," + longitude);
+
+		boolean MS = true;
+		boolean MS1 = false;
+		System.out.println("Person's marital status:" + MS + " or " + MS1);
+
+		String occupation = "Software Engineer";
+		System.out.println("Person's occupation: " + occupation);
+
+		String colour = "Blue";
+		System.out.println("Person's favourite colour:" + colour);
+
+		int year = 2023;
+		System.out.println("Current year:" + year);
+
+		int followers = 1000000;
+		System.out.println("Number of followers on a social media platform: " + followers);
+
+		float Rating = 7.5f;
+		System.out.println("Rating of a movie:  " + Rating);
+
+		String BloodGroup = "'A'";
+		System.out.println("Person's Blood Group is:" + BloodGroup);
+
+		
+		  String Book="\"To Kill a Mockingbird\"";
+		  System.out.println("Title of a book:"+Book);
+		  
+		  int count=500;
+		  System.out.println("Number of employees in a company:"+count);
+		  
+		  String Event="2:30 PM";
+		  System.out.println("Time of an event: "+Event);
+		  
+		  String Country="\"United States\"";
+		  System.out.println("Name of a country: "+Country);
+		  
+		  String Eye="\"Brown\"";
+		  System.out.println("Person's eye color:"+Eye);
+		  
+		  String birthplace="\"New York City\"";
+		  System.out.println("Person's birthplace:"+birthplace);
+		  
+		  
+		  float Distance = 200.5f;
+		  System.out.println("Distance between two cities: " + Distance);
+		  
+		 
+
 	}
 
 }
